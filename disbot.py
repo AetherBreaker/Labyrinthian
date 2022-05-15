e922f5011eaa248373eb755cc61128c721e49d09
AetherBreaker <deltajackal77@yahoo.com>
2022-05-15 01:10:24 -0400

    not sure what i changed, but it wasnt saved so

--- disbot.py
+++ disbot.py
@@ -1,6 +1,7 @@
 import disnake
 from disnake.ext import commands
 import logging
+from disnake import Embed
 
 logging.basicConfig(level=logging.INFO)
 
@@ -9,75 +10,75 @@ intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(
-    command_prefix="'",
-    test_guilds=[915674780303249449, 951225215801757716],
-    sync_commands_debug=True,
-    intents=intents,
-    owner_ids=['200632489998417929', '136583737931595777']
-    # In the list above you can specify the IDs of your test guilds.
-    # Why is this kwarg called test_guilds? This is because you're not meant to use
-    # local registration in production, since you may exceed the rate limits.
+	command_prefix="'",
+	test_guilds=[915674780303249449, 951225215801757716],
+	sync_commands_debug=True,
+	intents=intents,
+	owner_ids=['200632489998417929', '136583737931595777']
+	# In the list above you can specify the IDs of your test guilds.
+	# Why is this kwarg called test_guilds? This is because you're not meant to use
+	# local registration in production, since you may exceed the rate limits.
 )
 
 @bot.slash_command(description="Responds with 'World'")
 async def hello(inter):
-    await inter.response.send_message("World")
+	await inter.response.send_message("World")
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+	print(f'We have logged in as {bot.user}')
 
 validClasses = ['artificer', 'barbarian', 'bard', 'druid', 'cleric', 'ranger', 'fighter', 'paladin', 'blood hunter', 'sorcerer', 'warlock', 'wizard']
 nl = '\n'
 
 @bot.slash_command(description="Logs your characters badges.")
 async def badgelog(inter):
-    pass
+	pass
 
 @badgelog.sub_command(description="Creates a badge log for your character")
 async def create(
-    inter,
-    sheetlink: str,
-    starting_class: str,
-    starting_class_level: int,
-    multiclass1: str = None,
-    multiclass1_level: int = 0,
-    multiclass2: str = None,
-    multiclass2_level: int = 0,
-    multiclass3: str = None,
-    multiclass3_level: int = 0,
-    multiclass4: str = None,
-    multiclass4_level: int = 0
+	inter,
+	sheetlink: str,
+	starting_class: str,
+	starting_class_level: int,
+	multiclass1: str = None,
+	multiclass1_level: int = 0,
+	multiclass2: str = None,
+	multiclass2_level: int = 0,
+	multiclass3: str = None,
+	multiclass3_level: int = 0,
+	multiclass4: str = None,
+	multiclass4_level: int = 0
 ):
-    await inter.response.send_message(embed = disnake.Embed(
-        title=f"Creating {'username here'}'s badge log!",
-        description=f"""{sheetlink=}\n{starting_class=}\n{starting_class_level=}{nl+'multiclass1='+str(multiclass1) if multiclass1!=None else ''}{nl+'multiclass1_level='+str(multiclass1_level) if multiclass1_level!=0 else ''}{nl+'multiclass2='+str(multiclass2) if multiclass2!=None else ''}{nl+'multiclass2_level='+str(multiclass2_level) if multiclass2_level!=0 else ''}{nl+'multiclass3='+str(multiclass3) if multiclass3!=None else ''}{nl+'multiclass3_level='+str(multiclass3_level) if multiclass3_level!=0 else ''}{nl+'multiclass4='+str(multiclass4) if multiclass4!=None else ''}{nl+'multiclass4_level='+str(multiclass4_level) if multiclass4_level!=0 else ''}"""
-    ))
+	await inter.response.send_message(embed = Embed(
+		title=f"Creating {'username here'}'s badge log!",
+		description=f"""{sheetlink=}\n{starting_class=}\n{starting_class_level=}{nl+'multiclass1='+str(multiclass1) if multiclass1!=None else ''}{nl+'multiclass1_level='+str(multiclass1_level) if multiclass1_level!=0 else ''}{nl+'multiclass2='+str(multiclass2) if multiclass2!=None else ''}{nl+'multiclass2_level='+str(multiclass2_level) if multiclass2_level!=0 else ''}{nl+'multiclass3='+str(multiclass3) if multiclass3!=None else ''}{nl+'multiclass3_level='+str(multiclass3_level) if multiclass3_level!=0 else ''}{nl+'multiclass4='+str(multiclass4) if multiclass4!=None else ''}{nl+'multiclass4_level='+str(multiclass4_level) if multiclass4_level!=0 else ''}"""
+	))
 
 @create.autocomplete("starting_class")
 async def validclasses_autocomp(inter: disnake.CommandInteraction, string: str):
-    string = string.casefold()
-    return [chClass for chClass in validClasses if string in chClass.casefold()]
+	string = string.casefold()
+	return [chClass for chClass in validClasses if string in chClass.casefold()]
 
 @create.autocomplete("multiclass1")
 async def validclasses_autocomp(inter: disnake.CommandInteraction, string: str):
-    string = string.casefold()
-    return [chClass for chClass in validClasses if string in chClass.casefold()]
+	string = string.casefold()
+	return [chClass for chClass in validClasses if string in chClass.casefold()]
 
 @create.autocomplete("multiclass2")
 async def validclasses_autocomp(inter: disnake.CommandInteraction, string: str):
-    string = string.casefold()
-    return [chClass for chClass in validClasses if string in chClass.casefold()]
+	string = string.casefold()
+	return [chClass for chClass in validClasses if string in chClass.casefold()]
 
 @create.autocomplete("multiclass3")
 async def validclasses_autocomp(inter: disnake.CommandInteraction, string: str):
-    string = string.casefold()
-    return [chClass for chClass in validClasses if string in chClass.casefold()]
+	string = string.casefold()
+	return [chClass for chClass in validClasses if string in chClass.casefold()]
 
 @create.autocomplete("multiclass4")
 async def validclasses_autocomp(inter: disnake.CommandInteraction, string: str):
-    string = string.casefold()
-    return [chClass for chClass in validClasses if string in chClass.casefold()]
+	string = string.casefold()
+	return [chClass for chClass in validClasses if string in chClass.casefold()]
 
 bot.run('OTc0NTQyMTY0MjQ1NzU3OTcy.GdAvmG.DeKmgiiivoTUcrITPEUjm_E18wsKAJuQLx7sBs')
 
