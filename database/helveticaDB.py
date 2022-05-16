8dc5e88f4968cfff3f31e4d910b6efb1c7d33716
AetherBreaker <deltajackal77@yahoo.com>
2022-05-15 22:09:59 -0400

    Established database class.

---
+++ database/helveticaDB.py
@@ -0,0 +1,27 @@
+import disnake
+import os
+from disnake.ext import commands
+from dotenv import load_dotenv
+import pymongo
+from pymongo.errors import InvalidName
+
+
+class dbClient:
+	def __init__(self, database, collection):
+		c = pymongo.MongoClient(f"mongodb+srv://labyrinthadmin:{os.getenv('DBPSS')}@labyrinthdb.ng3ca.mongodb.net/helveticaDB?retryWrites=true&w=majority")
+		self.client = c
+		if database in c.list_database_names():
+			self.db = self.client[database]
+		else:
+			raise FileNotFoundError(f"'{database}' does not exist!")
+		if collection in self.db.list_collection_names():
+			self.collection = self.db[collection]
+		else:
+			raise FileNotFoundError(f"'{collection}' does not exist in the database!")
+
+	def insertDoc(self, collectionName, document, isMany):
+		if isMany:
+			self.collection.insert_many([document])
+		else:
+			self.collection.insert_one(document)
+
