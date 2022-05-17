05f3e11ea25ff5b474f092dcae03517cfe120120
AetherBreaker <deltajackal77@yahoo.com>
2022-05-16 22:19:08 -0400

    Packaged database initializer into bot class.

--- labyrinthianinit.py
+++ labyrinthianinit.py
@@ -4,7 +4,8 @@ import os
 from disnake.ext import commands
 from dotenv import load_dotenv
 import pymongo
-from database.helveticaDB import *
+import pymongo
+import motor.motor_asyncio
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -14,16 +15,24 @@ intents.message_content = True
 
 load_dotenv()
 
-db = dbClient("helveticaDB")
-
 extensions = [
 	"badgelogging.badgelog"
 ]
 
-bot = commands.Bot(
-	command_prefix="'",
-	test_guilds=[915674780303249449, 951225215801757716],
-	sync_commands_debug=True,
+class Labyrinthian(commands.Bot):
+	def __init__(self, prefix, **options):
+		super().__init__(
+			prefix,
+			test_guilds=[915674780303249449, 951225215801757716],
+			sync_commands_debug=True,
+			**options
+		)
+		self.mclient = motor.motor_asyncio.AsyncIOMotorClient(f"mongodb+srv://labyrinthadmin:{os.getenv('DBPSS')}@labyrinthdb.ng3ca.mongodb.net/?retryWrites=true&w=majority")
+		self.mdb = self.mclient["helveticaDB"]
+		#self.rdb = self.loop.run_until_complete(self.setup_rdb())
+
+bot = Labyrinthian(
+	prefix="'",
 	intents=intents,
 	owner_ids=['200632489998417929', '136583737931595777']
 )
