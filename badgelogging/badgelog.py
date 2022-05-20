588f8444bee33647e2b76fa01a112ec7eb7c364c
AetherBreaker <deltajackal77@yahoo.com>
2022-05-19 21:53:57 -0400

    Modified database references to reference collections unique to each server. Implemented commend for setting up a badge levelup template.

--- badgelogging/badgelog.py
+++ badgelogging/badgelog.py
@@ -3,9 +3,11 @@ from disnake.ext import commands
 from disnake import Embed
 import datetime
 from data.URLchecker import urlCheck
+import json
+from errors.errors import noValidTemplate
 
 #await cog_before_slash_command_invoke(inter):
-#		funcout = await commands.option_enum(self.bot.mdb['BLCharList'].find({"user": inter.author.id}).distinct({"character"}))
+#		funcout = await commands.option_enum(self.bot.sdb[f"BLCharList_{inter.guild.id}"].find({"user": inter.author.id}).distinct({"character"}))
 
 class Badges(commands.Cog):
 	def __init__(self, bot):
@@ -17,6 +19,26 @@ class Badges(commands.Cog):
 	@commands.slash_command(description="Log your characters badges.")
 	async def badges(self, inter: disnake.ApplicationCommandInteraction):
 		pass
+
+	@badges.sub_command(checks=[commands.has_permissions(administrator=True)])
+	async def template(self, inter, templatedict: str):
+		jason = {}
+		try:
+			jason = json.load(templatedict)
+		except noValidTemplate:
+			return await inter.response.send_message("Error: Template not a valid JSON")
+		for iter,x in enumerate(jason.keys()):
+			if iter > 20:
+				return await inter.response.send_message("Error: Template has too many entries")
+			elif x != str(iter):
+				return await inter.response.send_message("Error: Template keys are not a range of 1 through 20")
+		if all([isinstance(x, (int, float)) for x in jason.values()]):
+			jason.update({"setting": "bltemp"})
+			self.bot.sdb[f"srvconf_{inter.guild.id}"].replace_one({"setting": "bltemp"}, jason, True)
+		else:
+			return await inter.response.send_message("Error: Template value is not of type integer or float")
+			
+
 
 	#creates a master badge log entry, used for tracking data about a character and generating badge log entries
 	@badges.sub_command()
@@ -30,12 +52,11 @@ class Badges(commands.Cog):
 		startingclasslevel: The level of your character's starter class.
 		"""
 		if urlCheck(sheetlink):
-			character = await self.bot.mdb['BLCharList'].find_one({"user": str(inter.author.id), "character": charname})
-			print(str(character))
+			character = await self.bot.sdb[f"BLCharList_{inter.guild.id}"].find_one({"user": str(inter.author.id), "character": charname})
 			if character != None:
 				await inter.response.send_message(f"{charname}'s badge log already exists!")
 			else:
-				await self.bot.mdb['BLCharList'].insert_one({"user": str(inter.author.id), "sheet": sheetlink, "character": charname, "charlvl": startingclasslevel, "classes": {startingclass: 1}, "currentbadges": 0, "lastlog": None, "lastlogtime": datetime.datetime.now()})
+				await self.bot.sdb[f"BLCharList_{inter.guild.id}"].insert_one({"user": str(inter.author.id), "sheet": sheetlink, "character": charname, "charlvl": startingclasslevel, "classes": {startingclass: startingclasslevel}, "currentbadges": 0, "lastlog": None, "lastlogtime": datetime.datetime.now()})
 				await inter.response.send_message(f"Registered {charname}'s badge log with the Adventurers Coalition.")
 		else:
 			inter.response.send_message("Sheet type does not match accepted formats, or is not a valid URL.")
@@ -51,44 +72,47 @@ class Badges(commands.Cog):
 		----------
 		charname: The name of your character.
 		newname: Your characters new name."""
-		character = await self.bot.mdb['BLCharList'].find_one({"user": str(inter.author.id), "character": charname})
+		character = await self.bot.sdb[f"BLCharList_{inter.guild.id}"].find_one({"user": str(inter.author.id), "character": charname})
 		if character == None:
 			await inter.response.send_message(f"{charname} doesn't exist!")
 		else:
-			await self.bot.mdb['BLCharList'].update_one({"user":str(inter.author.id),"character":charname},{'$set':{"character": newname}})
+			await self.bot.sdb[f"BLCharList_{inter.guild.id}"].update_one({"user":str(inter.author.id),"character":charname},{'$set':{"character": newname}})
 			await inter.response.send_message(f"{charname}'s name changed to {newname}")
 
 	@rename.autocomplete("charname")
 	async def autocomp_charnames(self, inter: disnake.ApplicationCommandInteraction, user_input: str):
 		print(inter.author.id)
-		charlist = await self.bot.mdb['BLCharList'].distinct("character", {"user": str(inter.author.id)})
+		charlist = await self.bot.sdb[f"BLCharList_{inter.guild.id}"].distinct("character", {"user": str(inter.author.id)})
 		return [name for name in charlist if user_input.casefold() in name]
 
-	##master of the class sub commands
-	#@badges.sub_command_group(description="Set your characters classes in their badge log.")
-	#async def classes(self, inter):
-	#	pass
+	#master of the class sub commands
+	@badges.sub_command_group(description="Set your characters classes in their badge log.")
+	async def classes(self, inter):
+		pass
 
-	##adds a multiclass entry to a characters badgelog master
-	#@classes.sub_command()
-	#async def add(self, inter, charname: str, multiclassname: validClass, multiclasslevel: int):
-	#	"""Adds a multiclass to your characters badge log.
-	#	Parameters
-	#	----------
-	#	charname: The name of your character.
-	#	multiclassname: The class your multiclassing into.
-	#	multiclasslevel: The level of your new multiclass."""
-	#	character = await self.bot.mdb['BLCharList'].find_one({"user": str(inter.author.id), "character": charname})
-	#	if character == None:
-	#		await inter.response.send_message(f"{charname} doesn't exist!")
-	#	elif len(character['classes']) < 5:
-			
+	#adds a multiclass entry to a characters badgelog master
+	@classes.sub_command()
+	async def add(self, inter, charname: str, multiclassname: validClass, multiclasslevel: int):
+		"""Adds a multiclass to your characters badge log.
+		Parameters
+		----------
+		charname: The name of your character.
+		multiclassname: The class your multiclassing into.
+		multiclasslevel: The level of your new multiclass."""
+		character = await self.bot.sdb[f"BLCharList_{inter.guild.id}"].find_one({"user": str(inter.author.id), "character": charname})
+		if character == None:
+			await inter.response.send_message(f"{charname} doesn't exist!")
+		elif len(character['classes']) < 5:
+			character['classes'][multiclassname] = multiclasslevel
+			character['charlvl'] = sum(character['classes'].values())
+			await self.bot.sdb[f"BLCharList_{inter.guild.id}"].replace_one({"user": str(inter.author.id), "character": charname}, character)
+			await inter.response.send_message(f"{charname} multiclassed into {multiclassname}!")
 
-	#@add.autocomplete("charname")
-	#async def autocomp_charnames(self, inter: disnake.ApplicationCommandInteraction, user_input: str):
-	#	print(inter.author.id)
-	#	charlist = await self.bot.mdb['BLCharList'].distinct("character", {"user": str(inter.author.id)})
-	#	return [name for name in charlist if user_input.casefold() in name]
+	@add.autocomplete("charname")
+	async def autocomp_charnames(self, inter: disnake.ApplicationCommandInteraction, user_input: str):
+		print(inter.author.id)
+		charlist = await self.bot.sdb[f"BLCharList_{inter.guild.id}"].distinct("character", {"user": str(inter.author.id)})
+		return [name for name in charlist if user_input.casefold() in name]
 
 	##removes a multiclass entry from a characters badgelog master
 	#@classes.sub_command()
@@ -98,7 +122,7 @@ class Badges(commands.Cog):
 	#@remove.autocomplete("charname")
 	#async def autocomp_charnames(self, inter: disnake.ApplicationCommandInteraction, user_input: str):
 	#	print(inter.author.id)
-	#	charlist = await self.bot.mdb['BLCharList'].distinct("character", {"user": str(inter.author.id)})
+	#	charlist = await self.bot.sdb[f"BLCharList_{inter.guild.id}"].distinct("character", {"user": str(inter.author.id)})
 	#	return [name for name in charlist if user_input.casefold() in name]
 
 	##updatese a multiclass entry in a characters badgelog master
@@ -109,7 +133,7 @@ class Badges(commands.Cog):
 	#@update.autocomplete("charname")
 	#async def autocomp_charnames(self, inter: disnake.ApplicationCommandInteraction, user_input: str):
 	#	print(inter.author.id)
-	#	charlist = await self.bot.mdb['BLCharList'].distinct("character", {"user": str(inter.author.id)})
+	#	charlist = await self.bot.sdb[f"BLCharList_{inter.guild.id}"].distinct("character", {"user": str(inter.author.id)})
 	#	return [name for name in charlist if user_input.casefold() in name]
 
 	##returns a list of the invoking users character badge logs
@@ -129,7 +153,7 @@ class Badges(commands.Cog):
 	#@log.autocomplete("charname")
 	#async def autocomp_charnames(self, inter: disnake.ApplicationCommandInteraction, user_input: str):
 	#	print(inter.author.id)
-	#	charlist = await self.bot.mdb['BLCharList'].distinct("character", {"user": str(inter.author.id)})
+	#	charlist = await self.bot.sdb[f"BLCharList_{inter.guild.id}"].distinct("character", {"user": str(inter.author.id)})
 	#	return [name for name in charlist if user_input.casefold() in name]
 
 	##returns a log of the selected characters most recent log entries
@@ -140,7 +164,7 @@ class Badges(commands.Cog):
 	#@history.autocomplete("charname")
 	#async def autocomp_charnames(self, inter: disnake.ApplicationCommandInteraction, user_input: str):
 	#	print(inter.author.id)
-	#	charlist = await self.bot.mdb['BLCharList'].distinct("character", {"user": str(inter.author.id)})
+	#	charlist = await self.bot.sdb[f"BLCharList_{inter.guild.id}"].distinct("character", {"user": str(inter.author.id)})
 	#	return [name for name in charlist if user_input.casefold() in name]
 
 def setup(bot):
