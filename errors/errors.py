37380230feb57bfcbb90cb930c0e0aab6129a7e0
AetherBreaker <deltajackal77@yahoo.com>
2022-05-20 01:14:35 -0400

    Fixed ExternalImportError.

--- errors/errors.py
+++ errors/errors.py
@@ -7,5 +7,5 @@ class noValidTemplate(LabyrinthianException):
 		super().__init__("This is not a valid template")
 
 class ExternalImportError(LabyrinthianException):
-	def __init__(self):
-		super().__init__("Sheet type does not match accepted formats, or is not a valid URL.")
+	def __init__(self, msg):
+		super().__init__(msg)
