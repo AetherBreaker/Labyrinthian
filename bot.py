da5c09ffe37d1ca05e01478e02ee6aef3629fffb
AetherBreaker <deltajackal77@yahoo.com>
2022-06-13 18:29:17 -0400

    added func for getting server settings

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ from disnake.ext.commands.errors import CommandInvokeError
 from cogs.auction.auction_constructor import ConstSender
 from utils import MongoCache, config
 from utils.models.errors import LabyrinthianException
+from utils.settings.guild import ServerSettings
 
 if config.TESTING_VAR == "True":
     import sys
@@ -55,6 +56,12 @@ class Labyrinthian(commands.Bot):
         self.sdb: motor.motor_asyncio.AsyncIOMotorCollection = self.mclient[config.MONGODB_SERVERDB_NAME]
         self.dbcache = MongoCache.MongoCache(self, cwd, maxsize=50, ttl=20)
         self.charcache = MongoCache.CharlistCache(self, maxsize=50, ttl=20)
+
+    async def get_server_settings(self, guild_id: str):
+        if not isinstance(guild_id, str):
+            guild_id = str(guild_id)
+        server_settings = await ServerSettings.for_guild(self.dbcache, guild_id)
+        return server_settings
 
     # async def get_guild_prefix(self, guild: disnake.Guild) -> str:
     #     guild_id = str(guild.id)
