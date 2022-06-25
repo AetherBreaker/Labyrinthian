a0c3d341894854fd007f71739a50b6a15aaebb86
AetherBreaker <deltajackal77@yahoo.com>
2022-06-25 02:41:24 -0400

    Removed bad code.

--- utils/models/settings/__init__.py
+++ utils/models/settings/__init__.py
@@ -6,13 +6,6 @@ class SettingsBaseModel(BaseModel):
         validate_assignment = True
         smart_union = False
 
-    @classmethod
-    def from_dict(cls, *args, **kwargs):
-        return cls.parse_obj(*args, **kwargs)
-
-    def to_dict(self, *args, **kwargs):
-        return self.dict(*args, **kwargs)
-
 
 from .guild import ServerSettings  # noqa: E402
 
