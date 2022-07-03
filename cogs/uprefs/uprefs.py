abdc0e56122e55afe2fa7bf8639a131450e22bf4
AetherBreaker <deltajackal77@yahoo.com>
2022-07-03 02:06:11 -0400

    Cog framework for user preferences functionality

---
+++ cogs/uprefs/uprefs.py
@@ -0,0 +1,16 @@
+from typing import TYPE_CHECKING
+import disnake
+from disnake.ext import commands
+
+
+if TYPE_CHECKING:
+    from bot import Labyrinthian
+
+
+class UserPreferencesCog(commands.Cog):
+    def __init__(self, bot: "Labyrinthian"):
+        self.bot = bot
+
+
+def setup(bot: "Labyrinthian"):
+    bot.add_cog(UserPreferencesCog(bot))
