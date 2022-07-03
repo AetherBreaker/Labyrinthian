89f2efddecf6cb58319a080c78ec9a61fc1a8fed
AetherBreaker <deltajackal77@yahoo.com>
2022-07-03 02:07:50 -0400

    Modified pydantic url error template.

--- utils/models/settings/__init__.py
+++ utils/models/settings/__init__.py
@@ -5,6 +5,10 @@ class SettingsBaseModel(BaseModel):
     class Config:
         validate_assignment = True
         smart_union = False
+        # underscore_attrs_are_private = True
+        error_msg_templates = {
+            "value_error.url.scheme": "This is not an accepted sheet URL."
+        }
 
 
 from .guild import ServerSettings  # noqa: E402
