6f832600d95c7df2e4a2e53964d4845988518b0b
AetherBreaker <deltajackal77@yahoo.com>
2022-07-04 01:23:53 -0400

    Removed unecessary code.

--- utils/models/settings/__init__.py
+++ utils/models/settings/__init__.py
@@ -5,12 +5,9 @@ class SettingsBaseModel(BaseModel):
     class Config:
         validate_assignment = True
         smart_union = False
-        # underscore_attrs_are_private = True
         error_msg_templates = {
             "value_error.url.scheme": "This is not an accepted sheet URL."
         }
 
 
-from .guild import ServerSettings  # noqa: E402
-
-__all__ = ("ServerSettings", "CharacterSettings")
+__all__ = "ServerSettings"
