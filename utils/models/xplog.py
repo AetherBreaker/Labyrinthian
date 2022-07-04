dd05cb453e5978debbfacd4becb70cd84f28591d
AetherBreaker <deltajackal77@yahoo.com>
2022-07-04 01:38:31 -0400

    Implemented models for xp logs.

---
+++ utils/models/xplog.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+import math
+from typing import TYPE_CHECKING, Any, List, NewType
+
+from bson import ObjectId
+from utils.models import LabyrinthianBaseModel
+
+
+if TYPE_CHECKING:
+    ObjID = ObjectId
+    from pymongo.results import InsertOneResult
+else:
+    ObjID = Any
+
+
+UserID = NewType("UserID", str)
+DMUID = NewType("DMUID", str)
+GuildID = NewType("GuildID", str)
+CharacterName = NewType("CharacterName", str)
+
+
+class XPLogEntry(LabyrinthianBaseModel):
+    charref: ObjID
+    user: UserID
+    guild: GuildID
+    name: CharacterName
+    prevxp: float
+    xpadded: float
+    dm: DMUID
+    timestamp: datetime
+
+    # ==== database ====
+    async def commit(self, db):
+        """Commits the settings to the database."""
+        data = self.dict()
+        result: "InsertOneResult" = await db.insert_one("xplog", data)
+        return result
+
+
+class XPLogBook(LabyrinthianBaseModel):
+    log: List[XPLogEntry]
+
+    # ==== construction ====
+    @classmethod
+    async def new(cls, db, char_ref_id: ObjectId) -> "XPLogBook":
+        data = (
+            db["xplog"]
+            .find({"charref": char_ref_id}, sort=("timestamp", -1))
+            .to_list(None)
+        )
+        return cls.construct(log=[XPLogEntry.construct(values=x) for x in data])
+
+    def paginate(self, items_per_page: int):
+        data = [*self.log]
+        for x in range(math.ceil(len(self.log) / items_per_page)):
+            result = []
+            for y in range(items_per_page):
+                if not data:
+                    yield result
+                    break
+                else:
+                    result.append(data[0])
+                    data = data[1:]
+            if not data:
+                break
+            yield result
